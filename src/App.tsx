47e2e2acb90dc197caf843f66f76971f4b8a14df
rimao416 <omarkayumba12345@gmail.com>
2024-08-02 12:08:40 +0100

    Loader

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,29 @@ import "./main.scss";
 import "./App.scss";
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
-
+import { useEffect, useState } from "react";
+import Preloader from "./components/PreLoader";
 function App() {
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    // Fonction de chargement des assets
+    const loadAssets = (): Promise<void> => {
+      return new Promise((resolve) => {
+        setTimeout(() => {
+          resolve();
+        }, 3000); // Simulez 3 secondes de chargement
+      });
+    };
+
+    // Charger les assets et mettre à jour l'état de chargement
+    loadAssets().then(() => {
+      setLoading(false);
+    });
+  }, []);
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      l
+      <Route path="/" element={loading ? <Preloader/>:<Home/>} />
     
     </Routes>
   );
